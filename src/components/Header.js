f68416705579981174038db2d49feda14727ab85
GhostCodeKC <andrewmgrube@gmail.com>
2020-10-24 19:19:09 -0500

    Initialize App

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+    render(){
+    
+        return (
+            <header id="home">
+            <nav id="nav-wrap">
+            <h1>HELLO!</h1>
+            </nav>
+            </header>
+        )
+
+    }
+}
+export default Header;
